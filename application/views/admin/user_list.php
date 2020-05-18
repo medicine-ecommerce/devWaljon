96ccdd219821d0dec9f9a41996fb858c17e7625f
mausam-varun007 <mausam.varun22@gmail.com>
2020-05-18 19:16:52 +0530

    test commit

---
+++ application/views/admin/user_list.php
@@ -0,0 +1,146 @@
+
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+            <div class="page-title">
+              <div class="title_left">
+                <h3>Users <small>Some examples to get you started</small></h3>
+              </div>
+            </div>
+
+            <div class="col-md-12 col-sm-12 ">
+              <div class="x_panel">
+                <div class="x_title">
+                  <h2>Doctors<small>List</small></h2>
+                  
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <div class="card-box table-responsive">                
+                        <table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
+                          <thead>
+                            <tr>
+                              <th>First name</th>
+                              <th>Last name</th>
+                              <th>Position</th>
+                              <th>Office</th>
+                              <th>Age</th>
+                              <th>Start date</th>
+                              <th>Salary</th>
+                              <th>Extn.</th>
+                              <th>E-mail</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td>Tiger</td>
+                              <td>Nixon</td>
+                              <td>System Architect</td>
+                              <td>Edinburgh</td>
+                              <td>61</td>
+                              <td>2011/04/25</td>
+                              <td>$320,800</td>
+                              <td>5421</td>
+                              <td>t.nixon@datatables.net</td>
+                            </tr>
+                            <tr>
+                              <td>Jennifer</td>
+                              <td>Acosta</td>
+                              <td>Junior Javascript Developer</td>
+                              <td>Edinburgh</td>
+                              <td>43</td>
+                              <td>2013/02/01</td>
+                              <td>$75,650</td>
+                              <td>3431</td>
+                              <td>j.acosta@datatables.net</td>
+                            </tr>
+                            <tr>
+                              <td>Cara</td>
+                              <td>Stevens</td>
+                              <td>Sales Assistant</td>
+                              <td>New York</td>
+                              <td>46</td>
+                              <td>2011/12/06</td>
+                              <td>$145,600</td>
+                              <td>3990</td>
+                              <td>c.stevens@datatables.net</td>
+                            </tr>
+                            <tr>
+                              <td>Hermione</td>
+                              <td>Butler</td>
+                              <td>Regional Director</td>
+                              <td>London</td>
+                              <td>47</td>
+                              <td>2011/03/21</td>
+                              <td>$356,250</td>
+                              <td>1016</td>
+                              <td>h.butler@datatables.net</td>
+                            </tr>
+                            <tr>
+                              <td>Lael</td>
+                              <td>Greer</td>
+                              <td>Systems Administrator</td>
+                              <td>London</td>
+                              <td>21</td>
+                              <td>2009/02/27</td>
+                              <td>$103,500</td>
+                              <td>6733</td>
+                              <td>l.greer@datatables.net</td>
+                            </tr>
+                            <tr>
+                              <td>Jonas</td>
+                              <td>Alexander</td>
+                              <td>Developer</td>
+                              <td>San Francisco</td>
+                              <td>30</td>
+                              <td>2010/07/14</td>
+                              <td>$86,500</td>
+                              <td>8196</td>
+                              <td>j.alexander@datatables.net</td>
+                            </tr>
+                            <tr>
+                              <td>Shad</td>
+                              <td>Decker</td>
+                              <td>Regional Director</td>
+                              <td>Edinburgh</td>
+                              <td>51</td>
+                              <td>2008/11/13</td>
+                              <td>$183,000</td>
+                              <td>6373</td>
+                              <td>s.decker@datatables.net</td>
+                            </tr>
+                            <tr>
+                              <td>Michael</td>
+                              <td>Bruce</td>
+                              <td>Javascript Developer</td>
+                              <td>Singapore</td>
+                              <td>29</td>
+                              <td>2011/06/27</td>
+                              <td>$183,000</td>
+                              <td>5384</td>
+                              <td>m.bruce@datatables.net</td>
+                            </tr>
+                            <tr>
+                              <td>Donna</td>
+                              <td>Snider</td>
+                              <td>Customer Support</td>
+                              <td>New York</td>
+                              <td>27</td>
+                              <td>2011/01/25</td>
+                              <td>$112,000</td>
+                              <td>4226</td>
+                              <td>d.snider@datatables.net</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /page content -->
