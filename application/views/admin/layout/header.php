4fc72a4aa278d903c415ddc5c0ed75ba5022ea0a
mausam-varun007 <mausam.varun22@gmail.com>
2020-05-18 21:23:39 +0530

    work on header

--- application/views/admin/layout/header.php
+++ application/views/admin/layout/header.php
@@ -28,8 +28,51 @@
 
     <!-- Custom Theme Style -->
     <link href="<?php echo base_url(); ?>build/css/custom.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>build/css/Style.css" rel="stylesheet">
   </head>
 
   <body class="nav-md">
     <div class="container body">
       <div class="main_container">
+        <!-- top navigation -->
+        <div class="top_nav">
+          <div class="nav_menu">
+              <div class="nav toggle">
+                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+              </div>
+              <nav class="nav navbar-nav">
+              <ul class=" navbar-right">
+                <li class="nav-item dropdown open">
+                  <a href="javascript:;" class="user-profile" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
+                    <i class="fa fa-user"></i>
+                    <span class="head-title">Account</span>
+                  </a>
+                  <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item"  href="javascript:;"> Profile</a>
+                      <a class="dropdown-item"  href="javascript:;">
+                        <span class="badge bg-red pull-right">50%</span>
+                        <span>Settings</span>
+                      </a>
+                  <a class="dropdown-item"  href="javascript:;">Help</a>
+                    <a class="dropdown-item"  href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
+                  </div>
+                </li>
+
+                <li role="presentation" class="nav-item">
+                  <a href="javascript:;" class="info-number" aria-expanded="false">
+                    <i class="fa fa-commenting"></i>
+                    <span class="head-title">Support</span>
+                  </a>
+                </li>
+
+                <li role="presentation" class="nav-item">
+                  <a href="javascript:;" class="info-number" aria-expanded="false">
+                    <i class="fa fa-envelope-o"></i>
+                    <span class="head-title">Contact</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <!-- /top navigation -->
