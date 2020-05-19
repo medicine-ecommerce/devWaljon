09fa3fb675ed97889683521f04425d68d08f2a43
mausam-varun007 <mausam.varun22@gmail.com>
2020-05-19 22:54:23 +0530

    dashboard sidebar

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -22,5 +22,15 @@ class MY_Controller extends CI_Controller
       // $this->load->view('developer/index', $this->template);
 
     }
+    public function Vendor(){
+
+      $this->template['header']        = $this->load->view('admin/layout/header');      
+      $this->template['sidebar']        = $this->load->view('admin/layout/sidebar');  
+      $this->template['middle']        = $this->load->view('vendor/'.$this->middle);
+      $this->template['footer']        = $this->load->view('admin/layout/footer');
+      return  $this->template;
+      // $this->load->view('developer/index', $this->template);
+
+    }
 }
 ?>
---
+++ application/views/vendor/personalDetails.php
@@ -0,0 +1,28 @@
+<div class="right_col">
+	<div class="row padding-top50">
+		<div class="col-md-3">
+			<h5>Personal Information </h5>
+		</div>
+		<div class="col-md-9">
+			<hr class="custom-form-hr">
+		</div>		
+	</div>
+	<div class="row"> 
+		<div class="col-md-8">
+			<div class="col-md-3">
+				<input type="text" name="first_name">
+			</div>
+			<div class="col-md-3">
+				<input type="text" name="first_name">
+			</div>
+			<div class="col-md-3">
+				<input type="text" name="first_name">
+			</div>
+			<div class="col-md-3">
+				<input type="text" name="first_name">
+			</div>
+		</div>
+		<div class="col-md-4">
+		</div>
+	</div>
+</div>
