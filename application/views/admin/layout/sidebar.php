3e46765524d07bbb27f5a0b66641861e6b184fa2
mausam-varun007 <mausam.varun22@gmail.com>
2020-05-19 21:40:11 +0530

    dashboard sidebar

--- application/views/admin/layout/sidebar.php
+++ application/views/admin/layout/sidebar.php
@@ -1,5 +1,5 @@
 
-        <div class="col-md-3 left_col">
+        <div class="col-md-3 left_col custom-sidebar-col">
           <div class="left_col scroll-view admin-side-bar">
             <!-- <div class="navbar nav_title" style="border: 0;">
               <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
