75795dcf6afd30b70f02dbd6a4851a441af5fe64
mausam-varun007 <mausam.varun22@gmail.com>
2020-05-20 22:53:05 +0530

    personal details form

--- application/views/admin/layout/header.php
+++ application/views/admin/layout/header.php
@@ -37,8 +37,7 @@
     <!-- Custom Theme Style -->
     <link href="<?php echo base_url(); ?>build/css/custom.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>build/css/Style.css" rel="stylesheet">
-    <script src="<?php echo base_url(); ?>vendors/jquery/dist/jquery.min.js"></script>
-    <script src="//geodata.solutions/includes/countrystatecity.js"></script>
+    <script src="<?php echo base_url(); ?>vendors/jquery/dist/jquery.min.js"></script>    
   </head>
 
   <body class="nav-md">
