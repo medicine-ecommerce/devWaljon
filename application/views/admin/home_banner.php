c7e3f89151f14f1b99cb311e0b135b0d4ed9cd8a
mausam-varun007 <mausam.varun22@gmail.com>
2020-05-22 19:51:15 +0530

    work on home module page design

--- application/views/admin/home_banner.php
+++ application/views/admin/home_banner.php
@@ -1,8 +1,52 @@
+
   <!-- page content -->
   <div class="right_col" role="main">
-    <!-- top tiles -->
-    <div class="row">
-      
-    </div> 
+    <div class="">
+      <div class="page-title">
+        <div class="title_left">
+          <h3>Invertory > Home Modules</h3>
+        </div>
+      </div>
+
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+          <div class="x_content">
+            <div class="img-upload-page"> 
+              <div class="home-page-head">
+                <h4>Upload Advertisement Banners</h4>
+              </div> 
+              <div class="img-upload-div">
+              	<img src="">	
+              </div> 
+              <p>Banner Size 1920X600</p> 
+              <div class="img-upload-btn">
+              	<button type="button" class="btn active-btn btn-round">Upload</button>
+              	<span class="transh-btn"><i class="fa fa-trash"></i></span>	
+              </div>
+            </div>
+            <div class="previous-upload-img">
+            	<div class="home-page-head">
+                <h4>Previous Advertisement Banners </h4>
+              </div>
+              <ul>
+              	<li>
+              		<img src="">
+              	</li>
+              </ul> 
+            	<div class="pull-right">
+                <div class="submit-btn">
+                  <button type="button" class="btn previous-btn">Cancle</button>
+                  <button type="button" class="btn active-btn">Save</button>
+                </div> 
+              </div>	
+            </div>
+          </div>
+        </div>
+      </div> 
+    </div>
   </div>
   <!-- /page content -->
+
+      
+
+
