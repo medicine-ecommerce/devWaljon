ae562587ce9dc5a260b71ec0f3b4416530a69eee
mausam-varun007 <mausam.varun22@gmail.com>
2020-05-27 21:16:27 +0530

    edit profile page

--- application/views/vendor/vandorregister.php
+++ application/views/vendor/vandorregister.php
@@ -8,7 +8,6 @@
       
     </div>
     <div class="col-md-4">      
-      <?php echo base_url(); ?>
       <form id="registratio_form" method="post" action="<?php echo base_url(); ?>/vendor/vendorregister">
         <div class="form-content">
             <img src="<?php echo base_url(); ?>assets/img/snake2.png" class="registration-logo" id="preview">       
