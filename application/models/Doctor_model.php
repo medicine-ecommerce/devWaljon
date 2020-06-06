771e3a39e657e87837588e951b1ff87a9f9a9758
mausam-varun007 <mausam.varun22@gmail.com>
2020-06-06 17:39:55 +0530

    work doctor side

--- application/models/Doctor_model.php
+++ application/models/Doctor_model.php
@@ -10,7 +10,7 @@ class Doctor_model extends MY_model
  
 	public function login($data){		
 		$this->db->select('*');
-		$this->db->from('doctor');
+		$this->db->from('users');
 		$this->db->group_start();
 		$this->db->where('email',$data['email']);		
 		$this->db->or_where('mobile',$data['email']);
@@ -27,11 +27,12 @@ class Doctor_model extends MY_model
 	}
 	public function doctorRegistration($data)
 	{
-		return $this->insertData('doctor',$data);
+		return $this->insertData('users',$data);
 	}
 	public function doctorProfileUpdate($data,$where)
 	{
-		return $this->updateData('doctor',$data,$where);
+		return $this->updateData('users',$data,$where);
+
 	}
 	public function addBankAccount($data)
 	{
@@ -40,11 +41,11 @@ class Doctor_model extends MY_model
 
 	public function doctorList()
 	{
-		return $this->getData('doctor','*','');
+		return $this->getData('users','*','');
 	}
 	public function doctortatus($status,$id)
 	{
-		return $this->updateData('doctor',array('is_active'=>$status),array('id'=>$id));
+		return $this->updateData('users',array('is_active'=>$status),array('id'=>$id));
 	}
 
 }
