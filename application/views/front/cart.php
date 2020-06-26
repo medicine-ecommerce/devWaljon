37fd086f21819455dcd960773d3d6efa11283868
mausam-varun007 <mausam.varun22@gmail.com>
2020-06-26 20:46:48 +0530

    cart & delevery address page

---
+++ application/views/front/cart.php
@@ -0,0 +1,55 @@
+<style type="text/css">
+ 
+
+
+
+
+</style>
+<div class="container">
+  
+  
+  <div class="cart transition is-open">    
+    <div class="table">
+      <div class="layout-inline row th">
+        <div class="col col-pro">Product</div>
+        <div class="col col-price align-center "> 
+          Price
+        </div>
+        <div class="col col-qty align-center">QTY</div>
+        <div class="col">VAT</div>
+        <div class="col">Total</div>
+      </div>
+       <div class="layout-inline row">
+        
+        <div class="col col-pro layout-inline">
+          <img style="width: 50%;" src="http://desktop-backgrounds-org.s3.amazonaws.com/400x300/twitter-nature-high-definition.jpg" alt="kitten" />
+          <p>Curious Little Begger</p>
+        </div>
+        
+        <div class="col col-price col-numeric align-center ">
+          <p>₹59.99</p>
+        </div>
+
+        <div class="col col-qty layout-inline">
+          <a href="#" class="qty qty-minus">-</a>
+            <input type="numeric" value="3" style="margin-top: 35px;" />
+          <a href="#" class="qty qty-plus">+</a>
+        </div>
+        
+        <div class="col col-vat col-numeric">
+          <p>₹2.95</p>          
+        </div>
+         <div class="col col-total col-numeric">  
+           <p>₹182.95</p>
+         </div>         
+      </div>
+  
+       <div class="cart-total">
+         <span class="heading-total">Total Item</span><br><br>
+         <span class="heading-total">Total </span> 
+         </div>   
+    </div>
+    
+    <a href="#" class="btn btn-update">Update cart</a>
+  
+</div>
