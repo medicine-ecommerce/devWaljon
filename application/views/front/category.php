04edafd273e8fedfffe66a9c8fcc625223c34c65
mausam-varun <mausam@ssvtechnologies.in>
2020-07-05 17:39:22 +0530

    s

---
+++ application/views/front/category.php
@@ -0,0 +1,76 @@
+
+<section class="main-page">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">      
+       <?php require_once(APPPATH."views/front/category_sidebar_filter.php"); ?>
+      </div>
+      <div class="col-md-9" >
+        <div class="row">
+          <div class="col-md-3">            
+            <h3><a href="<?php echo base_url('user'); ?>">Home</a> <i class="fa fa-angle-right fa-custom"></i> <a href=""><?php if(!empty($main_category)){ echo $main_category->category_name;} ?></a></h3>           
+          </div>
+          <div class="col-md-9">  
+            <div class="left-pagination-section">              
+              <div class="category-pagination left-active">
+                <a class="firt_short active" href="javascript:voide(0)" data-srt="newest">Newest</a>
+                <a href="javascript:voide(0)" data-srt="newest">Popular</a>
+                <a href="javascript:voide(0)" data-srt="lowest">Low Price</a>
+                <a class="last_short " id="st4" data-srt="highest">High Price</a>
+              </div>
+              <!-- <div class="category-pagination right-pagination-section right-active">
+                <a  href="#"><i class="fa fa-chevron-left"></i></a>
+                <a class="active" href="#">1</a>
+                <a href="#">2</a>
+                <a href="#">3</a>
+                <a href="#">...</a>
+                <a href="#">6</a>
+                <a href="#"><i class="fa fa-chevron-right"></i></a>
+              </div> -->
+            </div>
+          </div>          
+        </div>
+        <div class="toolbox">                    
+        </div>
+        <div class="row product-section" id="dataList">  
+        </div>
+      </div>
+    </div>
+  </div> 
+</section>
+
+<script>
+  function searchFilter(page_num){
+      page_num = page_num?page_num:0;
+      var keywords = $('#keywords').val();
+      var sortBy = $('#sortBy').val();
+      $.ajax({
+          type: 'POST',
+          url: '<?php echo base_url('user/ajaxFilterData/'); ?>'+page_num,
+          data: $("#filter").serialize(),
+          beforeSend: function(){
+              $('.loading').show();
+          },
+          success: function(html){
+              $('#dataList').html(html);
+              $('.loading').fadeOut("slow");
+          }
+      });
+  }
+ searchFilter(0);
+ $('input[type=checkbox]').change(function () {
+    searchFilter(0);
+ });
+
+$('.left-active a').click(function(){                  
+            $('.left-active a').removeClass("active");
+           $(this).addClass("active");
+           $('input[name="sortby"]').val($(this).data('srt'));
+           searchFilter(0);
+      });
+      $('.right-active a').click(function(){                  
+            $('.right-active a').removeClass("active");
+           $(this).addClass("active");
+      });
+  
+</script>
---
+++ application/views/front/category_sidebar_filter.php
@@ -0,0 +1,184 @@
+<form id="filter">
+  <div class="panel-group filter-sidebar-section" id="accordionMenus" role="tablist" aria-multiselectable="true">
+    <input type="hidden" name="sortby" value="">
+    <div class="panel panel-default category-panel">
+      <div class="panel-heading" role="tab" id="headingTwo">
+        <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordionMenu" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+          <?php if(!empty($main_category)){ echo $main_category->category_name;} ?>
+        </a>
+      </h4>
+      </div>
+      <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+        <div class="panel-body category-filter-radio">
+          <div class="seacrh-section">            
+            <input id="brandInput" onkeyup="brandFilter()" placeholder="Search..." type="text" value="" />
+            <div class="underline"></div>   
+            <i class="fa fa-search custom-search" aria-hidden="true"></i>
+          </div>  
+          <div id="brandDiv">            
+            <?php            
+            if(!empty($category)) {
+              foreach ($category as $key => $value) { ?>
+                <div class="form-group">
+                  <input type="checkbox"  id="product_category_<?= $value->id ?>" value="<?= $value->id ?>" name="product_category_id[]" class="search_paramiter_checkbox" <?php if(!empty($selected_category_id)){ if($selected_category_id==$value->id){ echo "checked"; } }?> >
+                  <label for="product_category_<?= $value->id ?>" class="flt-checkbox-label"><?= $value->category_name; ?> </label>
+                </div>              
+            <?php } } ?>  
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default category-panel">
+      <div class="panel-heading" role="tab" id="headingTwo">
+        <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordionMenu" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Brands
+        </a>
+      </h4>
+      </div>
+      <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+        <div class="panel-body category-filter-radio">
+          <div class="seacrh-section">            
+            <input id="brandInput" onkeyup="brandFilter()" placeholder="Search..." type="text" value="" />
+            <div class="underline"></div>   
+            <i class="fa fa-search custom-search" aria-hidden="true"></i>
+          </div>  
+          <div id="brandDiv">            
+            <?php
+            if(!empty($brand)) {
+              foreach ($brand as $key => $value) { ?>
+                <div class="form-group">
+                  <input type="checkbox" id="brand<?= $value->id ?>" value="<?= $value->id ?>" name="brand_id[]" class="search_paramiter_checkbox" >
+                  <label for="brand<?= $value->id ?>" class="flt-checkbox-label"><?= $value->brand_name; ?> </label>
+                </div>              
+            <?php } } ?>  
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default category-panel">
+      <div class="panel-heading" role="tab" id="headingThree">
+        <h4 class="panel-title">
+          <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordionMenu" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+            Descount
+          </a>
+        </h4>
+      </div>
+      <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+        <div class="panel-body category-filter-radio">          
+
+          <div class="form-group">
+            <input type="checkbox" class="search_paramiter_checkbox" value="0-10" name="discount[]">
+            <label for="item" class="flt-checkbox-label">Less Than 10%</label>
+          </div>
+          <?php for ($i=10; $i < 110 ; $i+=10) {  ?>
+            <div class="form-group">
+              <input type="checkbox" value="<?php echo $i.'-'.($i+10) ?>" name="discount[]" class="search_paramiter_checkbox" >
+              <label for="item" class="flt-checkbox-label"><?php echo $i.'% and above';?></label>
+            </div>
+          <?php } ?>
+        </div>
+      </div>
+    </div>
+    
+    
+    <div class="panel panel-default category-panel">
+      <div class="panel-heading" role="tab" id="headingFour">
+        <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordionMenu" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+          Product Forms
+        </a>
+      </h4>
+      </div>
+      <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+        <div class="panel-body category-filter-radio">
+          <div class="seacrh-section">            
+            <input id="myInput" onkeyup="productFromFilter()" placeholder="Search..." type="text" />
+            <div class="underline"></div>   
+            <i class="fa fa-search custom-search" aria-hidden="true"></i>
+          </div>
+          <div id="productFrom">            
+            <?php
+            if(!empty($product_form)) {            
+              foreach ($product_form as $key => $value) { ?>
+                <div class="form-group">
+                  <input type="checkbox" id="product_form<?= $value->id ?>" value="<?= $value->id ?>" name="product_form[]" class="search_paramiter_checkbox" >
+                  <label for="product_form<?= $value->id ?>" class="flt-checkbox-label"><?= $value->name; ?> </label>
+                </div>              
+            <?php } } ?>            
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+</form>
+
+  <!-- <input type="text"  onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name">
+ -->
+<!-- <ul id="myUL">
+  <li><a href="#">Adele</a></li>
+  <li><a href="#">Agnes</a></li>
+
+  <li><a href="#">Billy</a></li>
+  <li><a href="#">Bob</a></li>
+
+  <li><a href="#">Calvin</a></li>
+  <li><a href="#">Christina</a></li>
+  <li><a href="#">Cindy</a></li>
+</ul> -->
+
+<script type="text/javascript">
+  
+  function productFromFilter() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("productFrom");
+    li = ul.getElementsByTagName("div");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("label")[0];
+        txtValue = a.textContent || a.innerText;        
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+// $('#myInput').blur(function(){
+//   var inputdata = $(this).val();
+//   if(inputdata!=''){    
+//     setInterval(function () {
+//     $("#myInput").val(inputdata);          
+//         }, 500);
+//   }
+  
+//     // $(this).val("hi");
+// });
+
+
+
+ function brandFilter() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("brandInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("brandDiv");
+    li = ul.getElementsByTagName("div");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("label")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+$(document).ready(function(){
+  $('.collapse').collapse()
+});
+
+
+</script>
