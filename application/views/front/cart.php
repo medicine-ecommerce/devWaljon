d383bf18c71779ffa2370ea153cdc75ce5849bac
mahig397 <mahima@ssvtechnologies.in>
2020-07-22 22:58:29 +0530

    payment integration

--- application/views/front/cart.php
+++ application/views/front/cart.php
@@ -2,20 +2,28 @@
   <div class="cart transition is-open">    
     <div class="table">
       <div class="layout-inline cart-heading row th ">
+        <div class="col col-pro"> No.</div>
         <div class="col col-pro">Product</div>
         <div class="col col-price align-center "> 
           Price
         </div>
         <div class="col col-qty align-center">QTY</div>
-        <div class="col">VAT</div>
+        <!-- <div class="col">VAT</div> -->
         <div class="col">Total</div>
       </div>
-      <?php       
+      <?php 
+      $total = 0;      
       if(!empty($this->cart->contents())){ 
-      foreach ($this->cart->contents() as $value) { ?>
-      <div class="layout-inline cart-content row">        
+        $i = 1 ;
+        
+      foreach ($this->cart->contents() as $value) {
+      $total = $total + $value['subtotal']; ?>
+      <div class="layout-inline cart-content row">
+        <div class="col col-vat">
+          <p><?php echo $i++; ?></p>          
+        </div>        
         <div class="col col-pro layout-inline">
-          <img style="width: 50%;" src="<?= base_url().$value['image']; ?>" alt="kitten" />
+          <img style="width: 50%;" src="<?= base_url().$value['image']; ?>">
           <p><?= $value['name']; ?></p>
         </div>        
         <div class="col col-price col-numeric align-center ">
@@ -28,17 +36,18 @@
             <button onclick="this.parentNode.querySelector('input[type=number]').stepUp(); updateCart(<?= $value['id']; ?>,this.parentNode.querySelector('input[value]'),'plus')" class="plus"></button>
           </div>          
         </div>        
-        <div class="col col-vat col-numeric">
+        <!-- <div class="col col-vat col-numeric">
           <p><i class='fas fa-rupee-sign'></i> 2.95</p>          
-        </div>
+        </div> -->
          <div class="col col-total col-numeric">  
-           <p><i class='fas fa-rupee-sign'></i><?= $value['subtotal'] ?></p>
+           <p><i class='fas fa-rupee-sign'></i><?php echo $value['subtotal'] ?></p>
          </div>         
       </div>  
-      <?php }}else{ echo "No product in cart"; } ?>
+      <?php }
+      }else{ echo "<h2><center>No product in cart</center></h2>"; } ?>
       <div class="cart-total cart-total-left">
-        <span class="heading-total">Total Item - <span class="total-qty"><?php echo count($this->cart->contents()); ?> </span> </span><br><br>
-        <span class="heading-total">Total </span> 
+        <!-- <span class="heading-total">Total Item - <span class="total-qty"><?php echo count($this->cart->contents()); ?> </span> </span><br><br> -->
+        <span class="heading-total">Total = <?php echo $total; ?> </span> 
       </div>         
     </div>
     <div class="row update-button">      
@@ -46,11 +55,11 @@
         <a href="<?php echo base_url('user/checkout');?>" class="btn btn-update">Checkout</a>
       </div>
     </div>
-    <div class="row checkout-button">      
+    <!-- <div class="row checkout-button">      
       <div class="col-md-12">        
         <a href="#" class="btn btn-update" onclick="location.reload();">Update cart</a>
       </div>
-    </div>
+    </div> -->
   </div>    
 </div>
   
